4a98aa8e0249566a7bd619dd0720ec6b8fe613c5
manya1632 <manyacs1303@gmail.com>
2024-07-11 01:24:14 +0530

    empty card styling

--- client/src/components/EmptyCard.jsx
+++ client/src/components/EmptyCard.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const EmptyCard = ({ message }) => {
   return (
-    <div className='flex items-center justify-center h-screen'>
-      <p className='w-1/3 text-sm font-medium text-slate-700 text-center leading-7 mb-40'>{message}</p>
+    <div className='flex items-center justify-center h-full'>
+      <p className='w-1/3 text-sm font-medium text-slate-700 text-center leading-7 mt-64'>{message}</p>
     </div>
   );
 };
