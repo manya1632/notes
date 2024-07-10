992f94b310dc82907b3a8078e5c21de8bc82b4bb
manya1632 <manyacs1303@gmail.com>
2024-07-10 18:55:08 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const DB_URL = process.env.DB_URL;
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'https://notes-app-mern-phi.vercel.app',
+    origin: "*",
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'], 
     credentials: true, 
